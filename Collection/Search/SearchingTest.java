715bef5ae945bb0e1d0a9c7620a451bba037df2f
Injae Lee <94044443+ginidino@users.noreply.github.com>
2022-01-17 18:13:01 +0900

    Add files via upload

--- Collection/Search/SearchingTest.java
+++ Collection/Search/SearchingTest.java
@@ -2,19 +2,21 @@ package Searching;
 
 import java.util.*;
 
-// binary search
 public class SearchingTest {
 	public static void main(String[] args) {
-		String key1 = "ssds";
-		String key2 = "abc";
-		String[] param = {key1, key2};
-		List<String> list = Arrays.asList(param);
-		Collections.sort(list);
-		
-		for (String s : list) {
-			System.out.println(s);
-		}
-		int pos = Collections.binarySearch(list, key2);
-		System.out.println(pos);
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(1);
+		list.add(2);
+        list.add(3);
+        list.add(10);
+        list.add(20);
+        
+        int index = Collections.binarySearch(list, 10);
+        System.out.println(index);
+        
+        index = Collections.binarySearch(list, 4);
+        System.out.println(index);
 	}
 }
+// print 3
+//       -4
