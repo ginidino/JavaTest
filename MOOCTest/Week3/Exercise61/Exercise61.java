36a9a08584d4bb2d6871acb5ef072fdc3db3b7a6
Injae Lee <94044443+ginidino@users.noreply.github.com>
2022-01-18 17:36:09 +0900

    Add files via upload

---
+++ MOOCTest/Week3/Exercise61/Exercise61.java
@@ -0,0 +1,14 @@
+package Test;
+
+import java.util.ArrayList;
+
+public class Exercise61 {
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("Hallo");
+		list.add("Ciao");
+		list.add("Hello");
+		System.out.println("There are this many items in the list:");
+		System.out.println(list.size());
+	}
+}
