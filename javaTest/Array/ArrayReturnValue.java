53dd1ae332a11c9961149ece9b892902415ebe6d
Injae Lee <94044443+ginidino@users.noreply.github.com>
2022-02-14 16:10:28 +0900

    Add files via upload

---
+++ javaTest/Array/ArrayReturnValue.java
@@ -0,0 +1,19 @@
+package test;
+
+public class ArrayReturnValue {
+	public static void main(String[] args) {
+		String[] teachers = giveStringTable();
+		for ( String teacher : teachers)
+	        System.out.println(teacher);
+	}
+
+	public static String[] giveStringTable() {
+		String[] tchrs = new String[3];
+		
+		tchrs[0] = "Bonus";
+	    tchrs[1] = "Ihq";
+	    tchrs[2] = "Lennon";
+	    
+		return tchrs;
+	}
+}
